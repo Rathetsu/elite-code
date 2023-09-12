e33e2e7da3eb479881fe23c2b0d649ba0813ab33
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-09-12 03:49:53 +0300

    initial implementation of navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>LeetClone</title>
+				<title>EliteCode</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<meta name='description' content='Inspired by LeetCode. Practice Competitve Programming Problems. Contains Solutions.' />
 				<link rel='icon' href='/ec_logo.ico' />
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+
+type AuthenticationPageProps = {};
+
+const AuthenticationPage: React.FC<AuthenticationPageProps> = () => {
+
+	return (
+		<div className='bg-gradient-to-b from-gray-600 to-black h-screen relative' >
+			<div className='max-w-7xl mx-auto px-4 sm:px-6'>
+				<NavBar />
+			</div>
+
+		</div>
+	);
+}
+
+export default AuthenticationPage;
