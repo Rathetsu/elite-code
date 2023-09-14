116b302d416f5a3f434ec7825f0aff0562b49e39
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-09-15 01:45:03 +0300

    implemented a customHook to handle closing the modals with the keyboard as well as the mouse

--- src/components/Modals/EliteModal.tsx
+++ src/components/Modals/EliteModal.tsx
@@ -1,5 +1,5 @@
 import { Sign } from 'crypto';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IoClose } from 'react-icons/io5';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
@@ -13,18 +13,18 @@ type EliteModalProps = {};
 const EliteModal: React.FC<EliteModalProps> = () => {
 
 	const modalState = useRecoilValue(authModalState);
-	const setModalState = useSetRecoilState(authModalState);
-
+	const closeModal = useCloseModal();
+	
 	return (
 		<>
-			<div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"></div>
+			<div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60" onClick={closeModal}></div>
 			<div className="w-full sm:w-[450px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center">
 				<div className="relative w-full h-full mx-auto flex items-center justify-center">
 					<div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6">
 						<div className="flex justify-end p-2">
 							<button className="bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center
 											hover:bg-gray-800 hover:text-white text-white">
-								<IoClose className="h-5 w-5" onClick={() => { setModalState((oldState) => ({ ...oldState, isOpen: false })) }} />
+								<IoClose className="h-5 w-5" onClick={closeModal} />
 							</button>
 						</div>
 
@@ -40,3 +40,29 @@ const EliteModal: React.FC<EliteModalProps> = () => {
 	);
 }
 export default EliteModal;
+
+
+function useCloseModal() {
+	const setModalState = useSetRecoilState(authModalState);
+
+	const closeModal = () => {
+		setModalState((oldState) => ({
+			...oldState,
+			isOpen: false,
+			activeModal: 'signIn',
+		}));
+	};
+
+	useEffect(() => {
+		window.addEventListener('keydown', (e) => {
+			if (e.key === 'Escape') {
+				closeModal();
+			}
+		});
+		return () => {
+			window.removeEventListener('keydown', () => { });
+		};
+	});
+
+	return closeModal;
+}
