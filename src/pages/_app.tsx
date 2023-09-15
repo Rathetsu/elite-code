60f1167d4334dc4032d0977d67ef3b1ded15e921
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-09-15 03:24:00 +0300

    updated app icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				<title>EliteCode</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<meta name='description' content='Inspired by LeetCode. Practice Competitve Programming Problems. Contains Solutions.' />
-				<link rel='icon' href='/ec_logo.ico' />
+				<link rel='icon' href='/e_icon.ico' />
 			</Head>
 			<Component {...pageProps} />
 		</RecoilRoot>
