917864936903f98028ed7e26e844f6495b0cef67
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-09-19 20:29:36 +0300

    Implemented the statements for 5 example problems + the testing handlers for them

---
+++ src/utils/types/problem.ts
@@ -0,0 +1,31 @@
+export type Example = {
+  id: number;
+  inputText: string;
+  outputText: string;
+  explanation?: string;
+  img?: string;
+};
+
+// local problem data
+export type Problem = {
+  id: string;
+  order: number;
+  title: string;
+  problemStatement: string;
+  examples: Example[];
+  constraints: string;
+  startingCode: string;
+  testingFunction: ((fun: any) => boolean) | string;
+  startingFunctionName: string;
+};
+
+export type DBProblem = {
+  id: string;
+  order: number;
+  title: string;
+  category: string;
+  difficulty: string;
+  likes: number;
+  dislikes: number;
+  solution?: string;
+};
