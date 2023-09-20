06f4ac8e606c8639ee0d75f9f3862e63330bc3d7
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-09-20 12:30:53 +0300

    SSR for static pages

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import TopBar from '@/components/TopBar/TopBar';
 import WorkSpace from '@/components/WorkSpace/WorkSpace';
+import { problems } from '@/utils/ProblemData';
+import { Problem } from '@/utils/types/problem';
 
-type ProblemPageProps = {};
+type ProblemPageProps = {
+	problem: Problem;
+};
 
-const ProblemPage: React.FC<ProblemPageProps> = () => {
+const ProblemPage: React.FC<ProblemPageProps> = ({problem}) => {
+
+	console.log(problem);
 
 	return (
 		<div>
-			<TopBar problemPage/>
-			<WorkSpace />
+			<TopBar problemPage />
+			<WorkSpace problem={problem} />
 		</div>
 	);
 }
 export default ProblemPage;
+
+export async function getStaticPaths() {
+	const paths = Object.keys(problems).map((prob) => ({
+		params: { pid: prob },
+	}));
+	return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }: { params: { pid: string } }) {
+	const { pid } = params;
+	const problem = problems[pid];
+
+	if (!problem) {
+		return {
+			notFound: true,
+		};
+	}
+
+	problem.testingFunction = problem.testingFunction.toString(); // turn function into string for serialization purposes
+
+	return {
+		props: {
+			problem,
+		},
+	};
+}
